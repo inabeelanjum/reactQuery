4ee305d54edef9116c05ec109fbd03a90dc6f602
Malik Nabeel Anjum <oinabeelay@gmail.com>
2022-08-20 20:25:54 +0500

    3rd argument

--- src/components/RqSuperheroes.js
+++ src/components/RqSuperheroes.js
@@ -2,24 +2,34 @@ import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 export const RqSuperheroes = () => {
-  const fetchh = () => {
-    return axios.get("http://localhost:4000/superheroes1");
+  const jeet = () => {
+    console.log("success query");
   };
-  const { data, isLoading, isError, error } = useQuery(["heroes"], fetchh);
-  if (isLoading) {
+  const fetchh = () => {
+    return axios.get("http://localhost:4000/superheroes");
+  };
+  const { data, isLoading, isError, error, isFetching, refetch } = useQuery(
+    ["heroes"],
+    fetchh,
+    {
+      enabled: false,
+      onSuccess: jeet(),
+    }
+  );
+  if (isFetching) {
     return <h1>loading...</h1>;
   }
   if (isError) {
     return <h1>{error.message}</h1>;
   }
-  if (data) {
-    return (
-      <>
-        <h2>Super Heroes react query</h2>
-        {data?.data.map((hero) => {
-          return <div>{hero.name}</div>;
-        })}
-      </>
-    );
-  }
+
+  return (
+    <>
+      <h2>Super Heroes react query</h2>
+      {data?.data.map((hero) => {
+        return <div key={hero.id}>{hero.name}</div>;
+      })}
+      <button onClick={refetch}>heroes </button>
+    </>
+  );
 };

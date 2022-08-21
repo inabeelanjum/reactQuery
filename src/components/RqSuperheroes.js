c1157bac6d49959769be1440b5b00d4dc23bf4ea
Malik Nabeel Anjum <oinabeelay@gmail.com>
2022-08-21 21:29:14 +0500

    parallel queries

--- src/components/RqSuperheroes.js
+++ src/components/RqSuperheroes.js
@@ -9,32 +9,53 @@ export const RqSuperheroes = () => {
   const fetchh = () => {
     return axios.get("http://localhost:4000/superheroes");
   };
-  const { data, isLoading, isError, error, isFetching, refetch } = useQuery(
-    ["heroes"],
-    fetchh,
-    {
-      enabled: false,
-      onSuccess: jeet(),
-    }
-  );
-  if (isFetching) {
+  const fetchhmore = () => {
+    return axios.get("http://localhost:4000/friends");
+  };
+  const heroes = useQuery(["heroes"], fetchh, {
+    enabled: false,
+    onSuccess: jeet(),
+  });
+  const friends = useQuery(["friends"], fetchhmore, {
+    enabled: false,
+    onSuccess: jeet(),
+  });
+
+  if (heroes.isFetching) {
     return <h1>loading...</h1>;
   }
-  if (isError) {
-    return <h1>{error.message}</h1>;
+  if (heroes.isError) {
+    return <h1>{heroes.error.message}</h1>;
+  }
+
+  if (friends.isFetching) {
+    return <h1>loading...</h1>;
+  }
+  if (friends.isError) {
+    return <h1>{heroes.error.message}</h1>;
   }
 
   return (
     <>
       <h2>Super Heroes react query</h2>
-      {data?.data.map((hero) => {
+      {heroes.data?.data.map((hero) => {
         return (
           <Link to={`/Hero/${hero.id}`}>
             <div key={hero.id}>{hero.name}</div>
           </Link>
         );
       })}
-      <button onClick={refetch}>heroes </button>
+      <button onClick={heroes.refetch}>heroes </button>
+
+      <h2>Super Heroes react query</h2>
+      {friends.data?.data.map((fri) => {
+        return (
+          <Link to={`/Hero/${fri.id}`}>
+            <div key={fri.id}>{fri.name}</div>
+          </Link>
+        );
+      })}
+      <button onClick={friends.refetch}>heroes </button>
     </>
   );
 };

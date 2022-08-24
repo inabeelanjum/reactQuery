02b8d607e78c880d45b33fe6c17a61947a2d52e1
Malik Nabeel Anjum <oinabeelay@gmail.com>
2022-08-24 16:18:37 +0500

    dynamic parallel queries done

--- src/components/DPquery.js
+++ src/components/DPquery.js
@@ -1,30 +1,51 @@
-// import React from "react";
+import React from "react";
 
-// import { useQueries } from "@tanstack/react-query";
-// import axios from "axios";
-// const DPquery = ({ heroIds }) => {
-//   console.log(heroIds);
-//   const fetchh = (id) => {
-//     const abc = axios.get(`http://localhost:4000/superheroes/${id}`);
-//     return abc;
-//   };
-//   const results = useQueries({
-//     queries: [
-//       { queryKey: ["hero", 1], queryFn: fetchh(1) },
-//       { queryKey: ["hero", 2], queryFn: fetchh(2) },
-//     ],
-//   });
-//   console.log({ results });
-//   if (results?.isLoading) {
-//     return <h1>loading...</h1>;
-//   }
-//   if (results?.isLoadingisError) {
-//     return <h1>{results.error.message}</h1>;
-//   }
-//   if (results?.isLoadingdata) {
-//     return <h1>hi nabeel</h1>;
-//   }
-//   return <h1>ok</h1>;
-// };
+import { useQueries } from "@tanstack/react-query";
+import axios from "axios";
+const DPquery = ({ heroIds }) => {
+  console.log(heroIds);
 
-// export default DPquery;
+  const fetchh = (id) => {
+    console.log("hi", id);
+    const abc = axios.get(`http://localhost:4000/superheroes/${id}`);
+    console.log("abc", abc);
+    return abc;
+  };
+  const basicF = (ai) => {
+    const a = ai.map((i) => {
+      return {
+        queryKey: ["post", i],
+        queryFn: () => fetchh(i),
+        staleTime: Infinity,
+      };
+    });
+    console.log("obj", a);
+    return a;
+  };
+  // basicF();
+  console.log("heroIds", heroIds);
+  const queriesList = heroIds.map((a) => {
+    return {
+      queryKey: ["post", a],
+      queryFn: () => fetchh(a),
+      staleTime: Infinity,
+    };
+  });
+  // : [];
+  const results = useQueries({
+    queries: basicF(heroIds),
+  });
+  console.log(results);
+  if (results?.isLoading) {
+    return <h1>loading...</h1>;
+  }
+  if (results?.isLoadingisError) {
+    return <h1>{results.error.message}</h1>;
+  }
+  if (results?.isLoadingdata) {
+    return <h1>hi nabeel</h1>;
+  }
+  return <h1>ok</h1>;
+};
+
+export default DPquery;
